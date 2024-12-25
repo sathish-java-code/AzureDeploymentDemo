a55896873a9cc0c001748903959c1c7c7fbd13be
sathish-java-code <sathishmadhu@hotmail.com>
2024-12-25 10:53:54 +0530

    AzureDemo initial Commit

---
+++ src/main/java/com/azuredemo/azuredemo/AzureController.java
@@ -0,0 +1,13 @@
+package com.azuredemo.azuredemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AzureController {
+
+    @GetMapping("/azure")
+    public String hello() {
+        return "Hello World";
+    }
+}
